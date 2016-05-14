1be97f67b9b5df0dd7aacc11d551699e4b9b2e5a
mml1 <mmlliguicota@gmail.com>
2016-05-13 19:55:59 -0700

    hb controller for slides

--- public/js/controllers/hungryBellyCtrl.js
+++ public/js/controllers/hungryBellyCtrl.js
@@ -1,9 +1,4 @@
-angular.module("projectsCtrl",[]).controller("ProjectsController", function($scope, $routeParams, ProjectFactory){
-	$scope.one = "";
-	$scope.project = {};
-	$scope.project.tech =[];
-
-
+angular.module("hungryBellysCtrl",[]).controller("HungryBellysController", function($scope, $routeParams, ProjectFactory){
 	$scope.myInterval = 3000;
   	$scope.slides = [
   		{id:0,image: "../../img/memoryLane/initial.png"},
@@ -12,26 +7,5 @@ angular.module("projectsCtrl",[]).controller("ProjectsController", function($sco
   		{id:3,image: "../../img/memoryLane/topten.png"}
 
   	];
-
-	ProjectFactory.index(function(projects){
-		$scope.projects = projects;
-	});
 		
-	// ProjectFactory.show($routeParams.id, function(info){
-	// 	$scope.one = info;
-	// });
-
-	//add a new project to my list of projects
-	$scope.add = function(){
-		console.log($scope.project);
-		// ProjectFactory.create($scope.project);
-	};
-
-	//retriev a project from db
-	$scope.retrieve = function(id){
-		ProjectFactory.show(id, function(info){
-			console.log("fact");
-			$scope.one = info;
-		});
-	};
 });
